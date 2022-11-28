f0ce98063b3d5335e43a4a3514610db590c9f83e
jasongao97 <jasongao97@gmail.com>
2022-11-28 10:17:14 -0500

    update test and docs

--- scripts/lib/test/embed.mjs
+++ scripts/lib/test/embed.mjs
@@ -7,12 +7,30 @@ test('Embedded Sketch', (t) => {
       {
         type: 'bookmark',
         bookmark: {
-          caption: [],
+          caption: [
+            {
+              type: 'text',
+              text: {
+                content: 'Figure 1.2',
+                link: null,
+              },
+              annotations: {
+                bold: false,
+                italic: false,
+                strikethrough: false,
+                underline: false,
+                code: false,
+                color: 'default',
+              },
+              plain_text: 'Figure 1.2',
+              href: null,
+            },
+          ],
           url: 'https://editor.p5js.org/natureofcode/sketches/5C69XyrlsR',
         },
       },
     ]),
-    '<div data-type="embed" data-p5-editor="https://editor.p5js.org/natureofcode/sketches/5C69XyrlsR"></div>',
+    '<figure><div data-type="embed" data-p5-editor="https://editor.p5js.org/natureofcode/sketches/5C69XyrlsR"></div><figcaption>Figure 1.2</figcaption></figure>',
     'should return an embedded example sketch',
   );
 
