26dde04c3cca81678fd7876345b8da7e50d6c6f9
Jason Gao <jasongao97@gmail.com>
2023-02-26 10:56:26 -0500

    remove previous scripts and dependencies

--- scripts/lib/handlers/equation.mjs
+++
@@ -1,8 +0,0 @@
-import { h } from 'hastscript';
-
-export function equation(block, parent) {
-  const node = h('div', { dataType: 'equation' }, block.equation.expression);
-  parent.children.push(node);
-
-  return null;
-}
