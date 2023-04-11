c84e0a0e6c35c02cedb1b13e8465c06ba6b0fe5a
shiffman <shiffman@users.noreply.github.com>
2023-04-11 12:53:52 +0000

    Notion - Update docs

--- content/examples/08_fractals/8_4_cantor_set/sketch.js
+++ content/examples/08_fractals/8_4_cantor_set/sketch.js
@@ -6,13 +6,12 @@
 // Renders a simple fractal, the Cantor Set
 
 function setup() {
-  createCanvas(640, 140);
+  createCanvas(640, 120);
   background(255);
-  stroke(0);
-  strokeWeight(2);
 
   // Call the recursive function
-  cantor(10, 20, width - 20);
+  strokeWeight(2);
+  cantor(10, 10, 620);
 }
 
 function draw() {
@@ -21,9 +20,9 @@ function draw() {
 }
 
 function cantor(x, y, length) {
+  //{!1} Stop at 1 pixel!
   if (length > 1) {
     line(x, y, x + length, y);
-    //{$1} Two recursive calls, note how 20 pixels are added to y
     cantor(x, y + 20, length / 3);
     cantor(x + (2 * length) / 3, y + 20, length / 3);
   }
