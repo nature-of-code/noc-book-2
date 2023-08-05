d8252c356dffa0d39350fc74a622f8548e657ebe
shiffman <shiffman@users.noreply.github.com>
2023-08-05 21:04:54 +0000

    Notion - Update docs

--- content/examples/01_vectors/example_1_1_bouncing_ball_with_no_vectors/sketch.js
+++ content/examples/01_vectors/example_1_1_bouncing_ball_with_no_vectors/sketch.js
@@ -3,6 +3,7 @@
 // http://natureofcode.com
 
 // Example 1-1: Bouncing Ball, no vectors
+// Variables for position and speed of ball.
 let x = 100;
 let y = 100;
 let xspeed = 2.5;
@@ -10,25 +11,27 @@ let yspeed = 2;
 
 function setup() {
   createCanvas(640, 240);
+  background(255);
 }
 
 function draw() {
   background(255);
 
-  // Add the current speed to the position.
+  // Move the ball according to its speed.
   x = x + xspeed;
   y = y + yspeed;
 
-  if ((x > width) || (x < 0)) {
+  //{!6} Check for bouncing.
+  if (x > width || x < 0) {
     xspeed = xspeed * -1;
   }
-  if ((y > height) || (y < 0)) {
+  if (y > height || y < 0) {
     yspeed = yspeed * -1;
   }
 
-  // Draw circle at x y position
   stroke(0);
-  strokeWeight(2);
   fill(127);
+  strokeWeight(2);
+  //{!1} Draw the ball at the position (x,y).
   circle(x, y, 48);
 }
--- content/examples/01_vectors/example_1_2_bouncing_ball_with_vectors/sketch.js
+++ content/examples/01_vectors/example_1_2_bouncing_ball_with_vectors/sketch.js
@@ -3,32 +3,32 @@
 // http://natureofcode.com
 
 // Example 1-2: Bouncing Ball, with p5.Vector!
+//{!2 .bold} Instead of a bunch of floats, we now just have two variables.
 let position;
 let velocity;
 
 function setup() {
   createCanvas(640, 240);
-  background(255);
+  //{!2 .bold} Note how createVector() has to be called inside of setup().
   position = createVector(100, 100);
-  velocity = createVector(2.5, 5);
+  velocity = createVector(2.5, 2);
 }
 
 function draw() {
   background(255);
-
-  // Add the current speed to the position.
+  //{!1 .bold .no-comment}
   position.add(velocity);
 
-  if ((position.x > width) || (position.x < 0)) {
+  //{!6 .bold .code-wide} We still sometimes need to refer to the individual components of a p5.Vector and can do so using the dot syntax: position.x, velocity.y, etc.
+  if (position.x > width || position.x < 0) {
     velocity.x = velocity.x * -1;
   }
-  if ((position.y > height) || (position.y < 0)) {
+  if (position.y > height || position.y < 0) {
     velocity.y = velocity.y * -1;
   }
 
-  // Display circle at x position
   stroke(0);
-  strokeWeight(2);
   fill(127);
-  ellipse(position.x, position.y, 48, 48);
+  strokeWeight(2);
+  circle(position.x, position.y, 48);
 }
