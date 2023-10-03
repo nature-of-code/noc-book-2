554004521436d5b2ad805e4f34dd44df39d1037c
Jason Gao <jasongao97@gmail.com>
2023-10-04 01:33:06 +0800

    add `chapterOpeningPagesOnly` config and adjust build configuration

--- magicbook/plugins/chapter-opening.js
+++ magicbook/plugins/chapter-opening.js
@@ -19,23 +19,27 @@ Plugin.prototype = {
 
         // match the number and name
         const match = /(\d+)\.\s([A-Za-z\s]+)/.exec(chapterTitle.text());
+        if (match) {
+          chapterTitle.replaceWith(
+            `<div class="chapter-opening"><div class="chapter-opening-header"><span class="chapter-number">${match[1]}</span><div class="chapter-opening-title"><h1>${match[2]}</h1></div></div></div>`,
+          );
 
-        chapterTitle.replaceWith(
-          `<div class="chapter-opening"><div class="chapter-opening-header"><span class="chapter-number">${match[1]}</span><div class="chapter-opening-title"><h1>${match[2]}</h1></div></div></div>`,
-        );
+          // if chapter-opening-quote exist
+          const chapterOpeningQuote = file.$el('div.chapter-opening-quote');
+          if (chapterOpeningQuote) {
+            file.$el('div.chapter-opening-title').append(chapterOpeningQuote);
+          }
 
-        // if chapter-opening-quote exist
-        const chapterOpeningQuote = file.$el('div.chapter-opening-quote');
-        if (chapterOpeningQuote) {
-          file.$el('div.chapter-opening-title').append(chapterOpeningQuote);
+          // if chapter-opening-figure exist
+          const chapterOpeningFigure = file.$el('div.chapter-opening-figure');
+          if (chapterOpeningFigure) {
+            file.$el('div.chapter-opening').append(chapterOpeningFigure);
+          }
         }
 
-        // if chapter-opening-figure exist
-        const chapterOpeningFigure = file.$el('div.chapter-opening-figure');
-        if (chapterOpeningFigure) {
-          file.$el('div.chapter-opening').append(chapterOpeningFigure);
+        if (config.chapterOpeningPagesOnly) {
+          file.$el('div.chapter-opening').nextAll().remove();
         }
-
         cb(null, file);
       }),
     );
