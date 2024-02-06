d2f3f62980a6ba4ca9c1c8b8458b03eb2bf2a2c4
jasongao97 <jasongao97@gmail.com>
2024-02-06 17:39:45 +0800

    set URLs style in italic

---
+++ magicbook/plugins/anchor.js
@@ -0,0 +1,32 @@
+const through = require('through2');
+const cheerio = require('cheerio');
+
+const Plugin = function (registry) {
+  registry.after('markdown:convert', 'anchor:url', this.addExternalLink);
+};
+
+Plugin.prototype = {
+  addExternalLink: function (config, stream, extras, callback) {
+    stream = stream.pipe(
+      through.obj(function (file, _, cb) {
+        file.$el = file.$el || cheerio.load(file.contents.toString());
+
+        file.$el('a').each(function () {
+          const anchor = file.$el(this);
+          const href = anchor.attr('href');
+          if (/^http/.exec(href)) {
+            anchor.append(` (<span class="url">${href}</span>)`);
+          }
+        });
+
+        file.contents = Buffer.from(file.$el.html());
+
+        cb(null, file);
+      }),
+    );
+
+    callback(null, config, stream, extras);
+  },
+};
+
+module.exports = Plugin;
