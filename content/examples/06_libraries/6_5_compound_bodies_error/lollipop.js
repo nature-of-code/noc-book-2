2a000dac9ae101beb3fcd79279206a3e2114281f
shiffman <shiffman@users.noreply.github.com>
2024-04-02 00:56:47 +0000

    Notion - Update docs

--- content/examples/06_libraries/6_5_compound_bodies_error/lollipop.js
+++ content/examples/06_libraries/6_5_compound_bodies_error/lollipop.js
@@ -25,48 +25,48 @@ class Lollipop {
 
   // Drawing the lollipop
   show() {
-    //     if (mouseIsPressed) {
-    //       // The angle comes from the compound body
-    //       let angle = this.body.angle;
+    if (mouseIsPressed) {
+      // The angle comes from the compound body
+      let angle = this.body.angle;
 
-    //       //{!2} Get the position for each part
-    //       let position1 = this.part1.position;
-    //       let position2 = this.part2.position;
+      //{!2} Get the position for each part
+      let position1 = this.part1.position;
+      let position2 = this.part2.position;
 
-    //       fill(127);
-    //       stroke(0);
-    //       strokeWeight(1);
+      fill(127);
+      stroke(0);
+      strokeWeight(1);
 
-    //       // Translate and rotate the rectangle (part1)
-    //       push();
-    //       translate(position1.x, position1.y);
-    //       rotate(angle);
-    //       rectMode(CENTER);
-    //       rect(0, 0, this.w, this.h);
-    //       pop();
+      // Translate and rotate the rectangle (part1)
+      push();
+      translate(position1.x, position1.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0, 0, this.w, this.h);
+      pop();
 
-    //       // Translate and rotate the circle (part2)
-    //       push();
-    //       translate(position2.x, position2.y);
-    //       rotate(angle);
-    //       fill(200);
-    //       circle(0, 0, this.r * 2);
-    //       pop();
-    //     } else {
-    let position = this.body.position;
-    let angle = this.body.angle;
-    rectMode(CENTER);
-    fill(127);
-    stroke(0);
-    strokeWeight(1);
-    push();
-    translate(position.x, position.y);
-    rotate(angle);
-    rect(0, 0, this.w, this.h);
-    fill(200);
-    circle(this.w / 2, 0, this.r * 2);
-    pop();
-    // }
+      // Translate and rotate the circle (part2)
+      push();
+      translate(position2.x, position2.y);
+      rotate(angle);
+      fill(200);
+      circle(0, 0, this.r * 2);
+      pop();
+    } else {
+      let position = this.body.position;
+      let angle = this.body.angle;
+      rectMode(CENTER);
+      fill(127);
+      stroke(0);
+      strokeWeight(1);
+      push();
+      translate(position.x, position.y);
+      rotate(angle);
+      rect(0, 0, this.w, this.h);
+      fill(200);
+      circle(this.w / 2, 0, this.r * 2);
+      pop();
+    }
   }
 
   checkEdge() {
