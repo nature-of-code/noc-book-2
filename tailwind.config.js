8ae881818b6a484b4b09a25aab4cae9d04402f24
Yifei Gao <jasongao97@gmail.com>
2024-04-15 19:46:18 -0400

    rewrite in tailwind style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      content: {
+        'icon-link':
+          "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='none' stroke='%23303030' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m9 15l6-6m-4-3l.463-.536a5 5 0 0 1 7.071 7.072L18 13m-5 5l-.397.534a5.07 5.07 0 0 1-7.127 0a4.97 4.97 0 0 1 0-7.071L6 11'/%3E%3C/svg%3E\")",
+      },
+      fontWeight: {
+        inherit: 'inherit',
+      },
       colors: {
         noc: {
           400: '#eb005a',
