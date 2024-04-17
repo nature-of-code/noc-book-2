3ff4f6e15cb948cb898ef54712e0778845c33add
Yifei Gao <jasongao97@gmail.com>
2024-04-17 14:41:08 -0400

    update examples page style

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { graphql, Link } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { FaBookOpen } from 'react-icons/fa';
 
 import p5jsLogo from '../images/p5js_logo.svg';
 import SideNavLayout from '../layouts/SideNavLayout';
@@ -8,38 +9,45 @@ import SideNavLayout from '../layouts/SideNavLayout';
 export default function ExamplesPage({ data }) {
   return (
     <SideNavLayout title="Examples">
-      {data.allBookSection.edges.map(({ node: chapter }) => {
+      {data.allBookSection.edges.map(({ node: chapter }, index) => {
         return (
           <section key={chapter.id}>
-            <h2 className="mt-0">Chapter {chapter.title}</h2>
+            <h2 className={`${index === 0 && 'mt-0'}`}>
+              Chapter {chapter.title}
+            </h2>
 
-            <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 xl:grid-cols-3">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
               {chapter.src.childrenExample.map((example) => {
                 const screenshot = getImage(example.screenshot);
+
+                const [exampleNumber, exampleName] = example.title.split(': ');
 
                 return (
                   <div key={example.id}>
                     <GatsbyImage
                       image={screenshot}
-                      className="aspect-[8/3] rounded border object-cover"
+                      className="aspect-[8/3] rounded-3xl border object-cover"
                       alt="p5.js sketch screenshot"
                     />
-                    <div className="not-prose py-2">
-                      <Link
-                        to={`/${chapter.slug}/#${example.slug}`}
-                        className="hover:underline"
-                      >
-                        <span className="font-bold">{example.title}</span>
-                      </Link>
-                      <br></br>
+                    <div className="not-prose mt-3 px-2">
+                      <div className="text-sm font-semibold">
+                        {exampleNumber}
+                      </div>
+                      <div className="text-sm">{exampleName}</div>
 
-                      <a href={example.webEditorURL}>
-                        <img
-                          src={p5jsLogo}
-                          alt="p5.js icon"
-                          className="inline-block h-10 w-8"
-                        ></img>
-                      </a>
+                      <div className="flex items-center gap-2">
+                        <Link to={`/${chapter.slug}/#${example.slug}`}>
+                          <FaBookOpen className="h-8 w-6 py-2 text-noc-400" />
+                        </Link>
+
+                        <a href={example.webEditorURL}>
+                          <img
+                            src={p5jsLogo}
+                            alt="p5.js icon"
+                            className="inline-block w-8 py-2"
+                          ></img>
+                        </a>
+                      </div>
                     </div>
                   </div>
                 );
