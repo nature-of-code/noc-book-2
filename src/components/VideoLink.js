100274adb9a4650de05c1e998825841e41c4739e
Yifei Gao <jasongao97@gmail.com>
2024-07-06 01:02:27 +0800

    update styles and <a> behavior

--- src/components/VideoLink.js
+++ src/components/VideoLink.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaYoutube } from 'react-icons/fa';
+import { FaYoutube, FaPlay } from 'react-icons/fa';
 
 const VideoLink = (props) => {
   return (
@@ -7,14 +7,18 @@ const VideoLink = (props) => {
       <a
         className="group relative flex items-center gap-2 text-base text-noc-400 no-underline"
         href={props['href']}
+        target="_blank"
+        rel="noreferrer"
       >
         <FaYoutube />
         {props['data-title']}
 
         {props.children && (
           <div className="not-prose absolute top-0 hidden w-80 pt-10 group-hover:block">
-            <div className="rounded-lg bg-noc-500 bg-opacity-50 p-4">
-              {props.children}
+            <div className="relative rounded-lg bg-noc-400 bg-opacity-50 p-4">
+              <div className="overflow-hidden rounded">{props.children}</div>
+
+              <FaPlay className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl text-noc-400" />
             </div>
           </div>
         )}
