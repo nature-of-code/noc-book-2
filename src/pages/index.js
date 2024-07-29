fd6b7ec0036f2bc492b06a0bf0b76fafb1f57c36
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-07-30 00:24:27 +0300

    change homepage to direct order

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,21 @@ export default function IndexPage() {
       <PurchaseButton className="my-6 lg:hidden" aligned="left" />
 
       <div className="my-6">
-      Hi hi hi! Welcome to The Nature of Code book! You can read the whole thing right here, thank you Creative Commons! If this project sparks joy and want to support it, you can <a href="https://github.com/sponsors/CodingTrain">sponsor on GitHub</a> or grab a copy of an artfully bound collection of processed cellulose fibers, imprinted with symbolic glyphs via pigment-based transfer particles, available at <a href="https://nostarch.com/nature-code">No Starch Press</a> and <a href="https://amzn.to/3ztc87a">other places where people buy books</a>. Don't forget to touch grass!
+        Hi hi hi! Welcome to The Nature of Code book! You can read the whole
+        thing right here, thank you Creative Commons! If this project sparks joy
+        and want to support it, you can{' '}
+        <a href="https://github.com/sponsors/CodingTrain">sponsor on GitHub</a>{' '}
+        or grab a copy of an artfully bound collection of processed cellulose
+        fibers, imprinted with symbolic glyphs via pigment-based transfer
+        particles. The best way to purchase is direct from me at{' '}
+        <a href="https://store.natureofcode.com/products/the-nature-of-code">
+          store.natureofcode.com
+        </a>{' '}
+        but you can also find it at{' '}
+        <a href="https://amzn.to/3ztc87a">
+          other places where people buy books
+        </a>
+        . Don't forget to touch grass!
       </div>
     </SideNavLayout>
   );
