6a552473a560ec671585f9712f9d835a4c74f9e4
Yifei Gao <jasongao97@gmail.com>
2024-08-26 00:08:47 +0800

    [feature] add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,70 @@
 import * as React from 'react';
+import { graphql, navigate } from 'gatsby';
 
-export default function NotFoundPage() {
+import BaseLayout from '../layouts/BaseLayout';
+import { getRandomItem } from '../utils';
+
+export default function NotFoundPage({ data }) {
+  const [sketch, setSketch] = React.useState(null);
+  const navigateToRandomChapter = () => {
+    const {
+      node: { slug },
+    } = getRandomItem(data.allBookSection.edges);
+    navigate(`/${slug}/`);
+  };
+
+  React.useEffect(() => {
+    const getSketches = async () => {
+      const sketches = await import('/static/404_sketches.json');
+      setSketch(sketches?.[0] && getRandomItem(sketches));
+    };
+
+    getSketches();
+  }, []);
+
   return (
-    <div className="px-4">
-      <div className="prose mx-auto my-8">
-        <h1>404</h1>
+    <BaseLayout>
+      <div className="mx-auto my-8 flex max-w-6xl flex-col items-center">
+        <h1 className="text-2xl font-bold">404 Page Not Found</h1>
+
+        {sketch && (
+          <>
+            <iframe
+              src={sketch.url}
+              className="mt-10 h-[644px] w-[802px] overflow-hidden rounded-xl border border-noc-400"
+            ></iframe>
+            <p className="mt-2">
+              <span>Credit: </span>
+              {sketch.name && <span>{sketch.name}</span>}
+              {sketch.socialMedia && (
+                <>
+                  <span className="mx-2 text-gray-400">|</span>
+                  <span>{sketch.socialMedia}</span>
+                </>
+              )}
+            </p>
+          </>
+        )}
+
+        <button
+          className="mt-12 rounded-xl bg-noc-400 px-4 py-2 text-sm font-medium text-white"
+          onClick={navigateToRandomChapter}
+        >
+          Bring Me To a Random Chapter!
+        </button>
       </div>
-    </div>
+    </BaseLayout>
   );
 }
+
+export const query = graphql`
+  query QueryTargetChapters {
+    allBookSection(filter: { type: { eq: "chapter" } }) {
+      edges {
+        node {
+          slug
+        }
+      }
+    }
+  }
+`;
