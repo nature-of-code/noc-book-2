934c5d49570171fd8af35662b7e3ccb1691f0824
Yifei Gao <jasongao97@gmail.com>
2024-11-04 22:17:17 +0800

    feat: add toc for pages
    keep the ability to override the headings

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -8,12 +8,10 @@ const customTableOfContents = {
     {
       id: 'ports-of-code-examples-to-other-languages',
       title: 'Code Ports',
-      level: 'h2',
     },
     {
       id: 'further-reading',
       title: 'Further Reading',
-      level: 'h2',
     },
   ],
 };
@@ -56,6 +54,12 @@ const PageItem = (props) => {
 
 const SideNav = (props) => {
   const { activeChapter = null, toc } = props;
+
+  // Only show the h2 headings as sub menus
+  const tableOfContent = toc
+    ? JSON.parse(toc).filter((heading) => heading.level === 'h2')
+    : [];
+
   const data = useStaticQuery(graphql`
     query QueryChaptersLink {
       allBookSection {
@@ -71,68 +75,65 @@ const SideNav = (props) => {
     }
   `);
 
-  const pages = data.allBookSection.edges
-    .filter(({ node }) => node.type === 'page')
-    .map(({ node }) => node);
-
-  const introductionPageIndex = pages.findIndex(
-    (node) => node.title === 'Introduction',
-  );
-
-  const chapters = data.allBookSection.edges
-    .filter(({ node }) => node.type === 'chapter')
-    .map(({ node }) => node);
+  const pages = data.allBookSection.edges.map(({ node }) => node);
 
   return (
     <nav className="rounded-3xl border border-noc-200">
       <ul className="divide-y divide-noc-200">
-        {pages.slice(0, introductionPageIndex + 1).map((node) => {
-          return (
-            <PageItem
-              key={node.slug}
-              slug={node.slug}
-              title={node.title}
-              type={node.type}
-            />
-          );
-        })}
-
-        {chapters.map((node) => {
-          const [chapterNumber] = node.title.split('. ');
-
-          return (
-            <PageItem
-              key={node.slug}
-              slug={node.slug}
-              title={node.title}
-              type={node.type}
-            >
-              {node.type === 'chapter' && chapterNumber === activeChapter && (
-                <TableOfContents toc={JSON.parse(toc)} />
-              )}
-            </PageItem>
-          );
-        })}
-
-        {pages.slice(introductionPageIndex + 1).map((node) => {
-          return (
-            <PageItem
-              key={node.slug}
-              slug={node.slug}
-              title={node.title}
-              type={node.type}
-            >
-              {node.title === activeChapter &&
-                customTableOfContents[activeChapter] && (
+        {pages.map((node) => {
+          // Override the sub menu
+          if (customTableOfContents[activeChapter]) {
+            return (
+              <PageItem
+                key={node.slug}
+                slug={node.slug}
+                title={node.title}
+                type={node.type}
+              >
+                {node.title === activeChapter && (
                   <TableOfContents
                     toc={customTableOfContents[activeChapter]}
                     highlight={false}
                   />
                 )}
+              </PageItem>
+            );
+          }
+
+          if (node.type === 'chapter') {
+            // Chapters match their number
+            const [chapterNumber] = node.title.split('. ');
+
+            return (
+              <PageItem
+                key={node.slug}
+                slug={node.slug}
+                title={node.title}
+                type={node.type}
+              >
+                {chapterNumber === activeChapter && (
+                  <TableOfContents toc={tableOfContent} />
+                )}
+              </PageItem>
+            );
+          }
+
+          // Pages match their na
+          return (
+            <PageItem
+              key={node.slug}
+              slug={node.slug}
+              title={node.title}
+              type={node.type}
+            >
+              {node.title === activeChapter && tableOfContent.length > 0 && (
+                <TableOfContents toc={tableOfContent} highlight={false} />
+              )}
             </PageItem>
           );
         })}
 
+        {/* Custom Pages: Examples / Exercises */}
         <PageItem key="example" slug="examples" title="Examples" />
         <PageItem key="exercise" slug="exercises" title="Exercises" />
       </ul>
